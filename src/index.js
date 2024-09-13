21a9a195676ccd09724afbc09a5f1352e23b7ad6
BeomSu Kim <33516980+Indigochi1d@users.noreply.github.com>
2024-09-13 22:43:28 +0900

    feat: 판정결과 문자열로 변경 및 정답여부 확인 구현

--- src/index.js
+++ src/index.js
@@ -23,10 +23,18 @@ export default class BaseballGame {
 
     // 사용자의 입력 유효성 확인
     checkUsersValidation(userInput) {
-        if (!this.checkUserInputLength(userInput)) return false;
-        if (!this.checkUserInputIsNumber(userInput)) return false;
-        if (!this.checkUserInputNumberRange(userInput)) return false;
-        if (!this.checkUserInputIsDiff(userInput)) return false;
+        if (!this.checkUserInputLength(userInput)) {
+            return false;
+        }
+        if (!this.checkUserInputIsNumber(userInput)) {
+            return false;
+        }
+        if (!this.checkUserInputNumberRange(userInput)) {
+            return false;
+        }
+        if (!this.checkUserInputIsDiff(userInput)) {
+            return false;
+        }
         return true;
     }
 
@@ -48,11 +56,11 @@ export default class BaseballGame {
     // 유저의 숫자가 모두 1~9사이인지
     checkUserInputNumberRange(input) {
         for (let i = 0; i < input.length; i++) {
-            if (!(1 <= Number(input[i]) && Number(input[i]) <= 9)) {
-                return true
+            if (1 > Number(input[i]) || 9 < Number(input[i])) {
+                return false
             }
         }
-        return false;
+        return true;
     }
 
     // 유저의 숫자가 모두 다른 숫자인지
@@ -67,11 +75,91 @@ export default class BaseballGame {
         for (let i = 0; i < 3; i++) {
             if (computerNumber[i] === userNumber[i]) {
                 score[1] += 1;
-            }
-            if (computerNumber.includes(userNumber[i])){
+            } else if (computerNumber.includes(userNumber[i])) {
                 score[0] += 1;
             }
         }
         return score;
     }
+
+    //판정결과를 문자열로 변경
+    play(userNumber, computerNumber) {
+        const score = this.checkingBallOrStrike(userNumber, computerNumber);
+        if (score[0] === 0 && score[1] === 0) {
+            return "낫싱";
+        } else if (score[0] === 0 && score[1] > 0) {
+            return `${score[1]}스트라이크`;
+        } else if (score[0] > 1 && score[0] === 0) {
+            return `${score[0]}볼`;
+        } else {
+            return `${score[0]}볼 ${score[1]}스트라이크`;
+        }
+    }
+
+    //정답 여부 확인
+    checkCorrectAnswer(result) {
+        return result === "3스트라이크";
+    }
+
+    // 정답 여부에 따라 판정결과 변경
+    printResult(answer) {
+        const correctness = this.checkCorrectAnswer(answer);
+
+        if (correctness) {
+            answer = "🎉정답을 맞추셨습니다🎉" + this.restartPart();
+            const submitBtn = document.querySelector("#submit");
+            submitBtn.disabled = true;
+        }
+        return answer;
+    }
+
+    // HTML로 출력
+    paintResult(result) {
+        const resultContainer = document.querySelector("#result");
+        resultContainer.innerHTML = this.printResult(result);
+    }
+
+    // 재시작 준비
+    restartPart() {
+        const restartBtn = `<button id="game-restart-button">재시작</button>`;
+        return `<div>게임을 다시 시작하시겠어요? <br/>${restartBtn}</div>`
+    }
+
+    // 재시작 버튼 기능 부여
+    giveFunctionToRestartBtn() {
+        const restartBtn = document.getElementById("game-restart-button");
+        restartBtn.addEventListener("click", this.restart);
+    }
+
+    restart() {
+        const input = document.querySelector("#user-input");
+        const submitBtn = document.querySelector("#submit");
+        const result = document.querySelector("#result");
+
+        input.value = null;
+        submitBtn.disabled = false;
+        result.innerHTML = "";
+
+        return new BaseballGame();
+    }
+
+    gameStart() {
+        const input = document.querySelector("#user-input");
+        const submitBtn = document.querySelector("#submit");
+
+        let computerRandomNumbers = this.makeRandomNumbers();
+
+        submitBtn.addEventListener("click", (event) => {
+            event.preventDefault();
+            if (!this.checkUsersValidation(input.value)) {
+                return alert("1~9사이의 서로 다른 세자리의 숫자를 입력해주세요!");
+            }
+            const result = this.play(input.value, computerRandomNumbers);
+            this.paintResult(result);
+            if (this.checkCorrectAnswer(result)) {
+                this.giveFunctionToRestartBtn();
+            }
+        })
+    }
 }
+new BaseballGame();
