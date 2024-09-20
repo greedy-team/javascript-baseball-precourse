c7cfb225b5a420f1a0fca697b7010da407a56ba0
Yeeun Park <ye6194@naver.com>
2024-09-20 17:34:44 +0900

    feat: 잘못된 입력값 처리

--- src/index.js
+++ src/index.js
@@ -4,9 +4,19 @@ const input = document.getElementById("user-input");
 function getUserInput(event) {
   event.preventDefault();
   let userInput = input.value;
-  console.log("userInput", userInput);
+  userInput = validateInput(userInput);
+  console.log("유효한 숫자인지 검사", userInput);
 
   return userInput;
+}
+
+function validateInput(userInput) {
+  const checkedInput = Number(userInput);
+
+  if (isNaN(checkedInput) || userInput[0] === "0" || userInput.length != 3) {
+    alert("1~9로 이루어진 세 자리 숫자를 입력해주세요.");
+    location.reload(); // 새로고침
+  } else return checkedInput;
 }
 
 submitBtn.addEventListener("click", getUserInput);
