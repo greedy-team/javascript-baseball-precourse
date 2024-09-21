76b21122d83fcb5d794a3df9198ba48daa1988a8
gogo1414 <wnstn6945@gmail.com>
2024-09-21 18:02:46 +0900

    feature: 게임 결과를 화면의 결과에 출력한다.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,39 @@
   export default class BaseballGame {
 
+    // BaseballGame 생성자
+    constructor() {
+      this.gameStart();
+    }
+
+    // 게임의 결과인 볼, 스트라이크, 낫싱 반환
+    play(computerNumbers, userNumbers) {
+      const results = this.compareNumbers(userNumbers, computerNumbers);
+
+      if(results[0] === 0 && results[1] === 0)
+        return "낫싱";
+      else if(results[0] === 0 && results[1] !== 0)
+        return results[1] + "볼";
+      else if(results[0] !== 0 && results[1] === 0)
+        return results[0] + "스트라이크";
+      else
+        return results[1] + "볼 " + results[0] + "스트라이크";
+    }
+
+    // 숫자 비교 후 볼, 스트라이크 확인
     compareNumbers(userNumbers, computerNumbers) {
-      let results = [0, 0, 0];
+      let results = [0, 0]; // 스트라이크, 볼
 
       for(let i = 0; i < 3; i++) {
         if(userNumbers[i] === computerNumbers[i]) 
           results[0]++;
-        else if(computerNumbers.includes())
+        else if(computerNumbers.includes(userNumbers[i]))
           results[1]++;
-      
-        return results;
       }
+
+      return results;
     }
 
+    // 컴퓨터 숫자 생성
     makeComputerNumbers() {
       let numbers = [];
 
@@ -30,10 +51,12 @@
       return numbers;
     }
 
+    // 1 ~ 9 랜덤 숫자 생성
     makeRandomNumber() {
       return MissionUtils.Random.pickNumberInRange(1, 9);
     }
 
+    // 컴퓨터의 서로 다른 3자리 숫자를 서로 다른지 검증
     validateComputerDifferentNumber(computerNumbers) {
       return new Set(computerNumbers).size === 3;
     }
@@ -65,26 +88,33 @@
       return true;
     }
 
+    printResult(result) {
+      const output = document.querySelector("#result");
+      output.insertAdjacentHTML('beforeend', `<div>${result}</div>`);
+    }
+
+    // 게임 시작
     gameStart() {
       const input = document.querySelector("#user-input");
       const submit = document.querySelector("#submit");
 
       submit.addEventListener('click', (event) => {
+        event.preventDefault(); // 기본 동작 방지
         const userInput = input.value;
         const userNumbers = userInput.split('').map(Number);
+
+        let computerNumbers = this.makeComputerNumbers();
 
         if (!this.validateLength(userNumbers) ||
             !this.validateNumbers(userInput) ||
             !this.validateDifferentNumber(userNumbers)) {
-          return this.gameStart(); // 검증 실패 시 재시작
+          return;
         }
-        
-        let computerNumbers = this.makeComputerNumbers();
 
-        alert('입력이 유효합니다!');
+        let result = this.play(userNumbers, computerNumbers);
+        this.printResult(result);
       });
     }
   }
 
-const game = new BaseballGame();
-game.gameStart();
+new BaseballGame();
