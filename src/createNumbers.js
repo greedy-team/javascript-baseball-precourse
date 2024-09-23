baa1b04e5443d36420f0b1bb36345dd4f73d9754
songhj10207@naver.com <songhj10207@naver.com>
2024-09-23 19:29:56 +0900

    feat: 랜덤 숫자 구현

---
+++ src/createNumbers.js
@@ -0,0 +1,13 @@
+export default function createNumber() {
+  const randomNumbers = [];
+
+  for (let i = 0; randomNumbers.length < 3; i++) {
+    const number = MissionUtils.Random.pickNumberInRange(1, 9);
+    if (!randomNumbers.includes(number)) {
+      randomNumbers.push(number);
+    }
+  }
+  const computerInput = randomNumbers.join("");
+
+  return computerInput;
+}
