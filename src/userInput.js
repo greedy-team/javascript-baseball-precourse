2176a7daa92a2f7b8faf396619ae61bb66601b9f
songhj10207@naver.com <songhj10207@naver.com>
2024-09-23 21:51:00 +0900

    fix: 문자열 따옴표 수정

--- src/userInput.js
+++ src/userInput.js
@@ -1,14 +1,14 @@
 export default function userInput(inputValue) {
-  const arrInput = inputValue.split("");
+  const arrInput = inputValue.split('');
   const set = new Set(arrInput);
   if (inputValue.length !== 3 || isNaN(inputValue)) {
-    window.alert("3자리 숫자로 입력하세요.");
+    window.alert('3자리 숫자로 입력하세요.');
     inputValue = null;
   } else if (set.size !== 3) {
-    window.alert("중복된 값입니다.");
+    window.alert('중복된 값입니다.');
     inputValue = null;
-  } else if (arrInput.includes("0")) {
-    window.alert("1에서 9까지의 숫자 입력하세요.");
+  } else if (arrInput.includes('0')) {
+    window.alert('1에서 9까지의 숫자 입력하세요.');
     inputValue = null;
   }
   return inputValue;
