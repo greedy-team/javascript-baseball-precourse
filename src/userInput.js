7f2648a0b0786745cf1d914cae3315570574b731
songhj10207@naver.com <songhj10207@naver.com>
2024-09-23 21:48:20 +0900

    fix: 함수 길이 작게 수정

--- src/userInput.js
+++ src/userInput.js
@@ -1,22 +1,15 @@
-export default function userInput() {
-  let isValidInput = false;
-  const inputValue = document.getElementById("user-input").value;
+export default function userInput(inputValue) {
   const arrInput = inputValue.split("");
   const set = new Set(arrInput);
-
   if (inputValue.length !== 3 || isNaN(inputValue)) {
     window.alert("3자리 숫자로 입력하세요.");
+    inputValue = null;
   } else if (set.size !== 3) {
     window.alert("중복된 값입니다.");
+    inputValue = null;
   } else if (arrInput.includes("0")) {
     window.alert("1에서 9까지의 숫자 입력하세요.");
-  } else {
-    isValidInput = true;
+    inputValue = null;
   }
-
-  if (isValidInput) {
-    return inputValue;
-  } else {
-    return null;
-  }
+  return inputValue;
 }
