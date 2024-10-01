8978784231945a884bdebe6f9d278ac0c4b8491e
Yeeun Park <ye6194@naver.com>
2024-10-01 14:18:58 +0900

    refactor: baseballGame() 함수를 BaseballGame 클래스로 변경

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { inputField, validateInput, validateUserInput } from "./validate.js";
+import {inputField, validateInput, validateUserInput} from "./validate.js";
 
 const submitBtn = document.getElementById("submit");
 const result = document.getElementById("result");
@@ -8,87 +8,75 @@ restartBtn.addEventListener("click", () => {
   location.reload();
 });
 
-submitBtn.addEventListener("click", BaseballGame);
-
-let computerInput;
-let randomNumber = [0, 0, 0];
-let pickedNumber;
-let userInput = "";
-let i;
-
-function BaseballGame() {
-  this.play = function (computerInput, userInput) {
-    let result = "";
-
-    getUserInput(event);
-    const userString = String(userInput);
-    const computerString = String(computerInput);
-
-    const [strikeCnt, ballCnt] = countStrikeBall(computerString, userString);
-
-    if (strikeCnt === 0 && ballCnt === 0) return (result = "낫싱");
-    if (strikeCnt === 3) return (result = "정답입니다!");
-    if (strikeCnt !== 0 && ballCnt === 0) return (result = `${strikeCnt}스트라이크`);
-    if (strikeCnt === 0 && ballCnt !== 0) return (result = `${ballCnt}볼`);
-
-    return (result = `${ballCnt}볼 ${strikeCnt}스트라이크`);
-  };
-
-  userInput = getUserInput(event);
-  if (userInput === "") return; // 잘못된 입력이 들어왔을 때는 경고창만 띄우고 아무것도 하지 않음
-
-  this.result = this.play(computerInput, userInput);
-
-  result.innerText = this.result; // 결과 표시
-  if (this.result === "정답입니다!") {
-    restartBtn.hidden = false; // 재시작 버튼 표시
-    submitBtn.disabled = true; // 확인 버튼 비활성화
-  }
-}
-
-// computerInput(정답)을 얻음
-randomNumber[0] = MissionUtils.Random.pickNumberInRange(1, 9);
-for (i = 1; i < 3; i++) {
-  pickedNumber = MissionUtils.Random.pickNumberInRange(1, 9);
-
-  // 이미 뽑은 숫자와 겹치는 숫자가 없는 경우
-  if (!randomNumber.includes(pickedNumber)) {
-    randomNumber[i] = pickedNumber;
-  } else i -= 1; // 이미 뽑은 숫자와 겹치는 숫자가 있는 경우
-}
-computerInput = randomNumber[0] * 100 + randomNumber[1] * 10 + randomNumber[2];
-console.log(computerInput);
-
-// userInput을 얻는 함수
-function getUserInput(event) {
-  event.preventDefault();
-
-  userInput = inputField.value;
-
-  if (!validateInput(userInput)) {
-    alert("1~9까지의 수를 중복없이 3개 입력해주세요.");
-    inputField.value = ""; // 입력창을 비움
-    return ""; // 잘못된 입력
+class BaseballGame {
+  constructor() {
+    this.result = "";
+    this.computerInput = this.getComputerInput();
   }
 
-  userInput = validateUserInput(userInput);
-  if (userInput === "") return userInput; // 잘못된 입력
-  return userInput;
-}
-
-// 스트라이크, 볼 개수를 세는 함수
-function countStrikeBall(computerString, userString) {
-  let strikeCnt = 0;
-  let ballCnt = 0;
-  let i;
-
-  for (i = 0; i < 3; i++) {
-    if (computerString[i] === userString[i]) {
-      strikeCnt += 1;
-      continue;
+  getComputerInput() {
+    let randomNumber = [0, 0, 0];
+    randomNumber[0] = MissionUtils.Random.pickNumberInRange(1, 9);
+    for (let i = 1; i < 3; i++) {
+      let pickedNumber = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!randomNumber.includes(pickedNumber)) randomNumber[i] = pickedNumber;
+      else i -= 1;
     }
-    if (computerString.includes(userString[i])) ballCnt += 1;
+    console.log(randomNumber[0] * 100 + randomNumber[1] * 10 + randomNumber[2]);
+    return randomNumber[0] * 100 + randomNumber[1] * 10 + randomNumber[2];
   }
 
-  return [strikeCnt, ballCnt];
+  getUserInput(event) {
+    event.preventDefault();
+
+    let userInput = inputField.value;
+    if (!validateInput(userInput)) {
+      alert("1~9까지의 수를 중복없이 3개 입력해주세요.");
+      inputField.value = "";
+      return "";
+    }
+    return validateUserInput(userInput);
+  }
+
+  countStrikeAndBall(computerString, userString) {
+    let strikeCnt = 0;
+    let ballCnt = 0;
+
+    for (let i = 0; i < 3; i++) {
+      if (computerString[i] === userString[i]) {
+        strikeCnt += 1;
+        continue;
+      }
+      if (computerString.includes(userString[i])) ballCnt += 1;
+    }
+    return [strikeCnt, ballCnt];
+  }
+
+  play(event) {
+    const userInput = this.getUserInput(event);
+    if (userInput === "") return;
+
+    const userString = String(userInput);
+    const computerString = String(this.computerInput);
+    const [strikeCnt, ballCnt] = this.countStrikeAndBall(computerString, userString);
+
+    if (strikeCnt === 0 && ballCnt === 0) return "낫싱";
+    if (strikeCnt === 3) return "정답입니다!";
+    if (strikeCnt !== 0 && ballCnt === 0) return `${strikeCnt}스트라이크`;
+    if (strikeCnt === 0 && ballCnt !== 0) return `${ballCnt}볼`;
+    return `${ballCnt}볼 ${strikeCnt}스트라이크`;
+  }
+
+  displayResult(event) {
+    this.result = this.play(event);
+    if (this.result !== undefined) result.innerText = this.result;  // 잘못된 입력이면 결과를 업데이트 하지 않음
+
+    if (this.result === "정답입니다!") {
+      restartBtn.hidden = false;
+      submitBtn.disabled = true;
+    }
+  }
 }
+
+const baseballGame = new BaseballGame();
+submitBtn.addEventListener("click", (event) => baseballGame.displayResult(event));
