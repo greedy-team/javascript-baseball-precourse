d557e048190e93d3be5ca2c4de3a878c6fda73c4
songhj10207@naver.com <songhj10207@naver.com>
2024-10-01 13:56:49 +0900

    feat: 사용자 입력 검증 함수 구현

---
+++ src/utils/verifyUserNumbers.js
@@ -0,0 +1,17 @@
+export default class verifyUserNumbers {
+  static validateNumber(numbers) {
+    const set = new Set(numbers);
+    if (numbers.length !== 3) {
+      window.alert('3자리 숫자로 입력하세요.');
+      return null;
+    } else if (
+      numbers.some((input) => !Number.isInteger(parseInt(input))) ||
+      numbers.includes('0') ||
+      set.size !== 3
+    ) {
+      window.alert('1에서 9까지 중복되지 않는 정수를 입력하세요');
+      return null;
+    }
+    return numbers;
+  }
+}
