510e71a192d27a17aa140da49cc9de4817564033
gogo1414 <wnstn6945@gmail.com>
2024-10-01 23:53:28 +0900

    refactor: 코드 줄 간격 맞추기

--- src/controller/BaseballGameController.js
+++ src/controller/BaseballGameController.js
@@ -4,66 +4,66 @@ import BaseballGame from "../model/BaseBallGame.js";
 
 export default class BaseballGameController {
 
-  constructor() {
-    this.NumberUtils = new NumberUtils();
-    this.BaseballGameView = new BaseballGameView();
-    this.BaseballGame = new BaseballGame();
-    this.gameStart();
-  }
-
-  endGame(result) {
-    setTimeout(function() {
-      alert(result + " 축하드립니다!");
-    }, 1);
-
-    const restartButton = document.querySelector("#game-restart-button");
-
-    this.BaseballGameView.toggleButtons(false);
-
-    restartButton.addEventListener('click', () => this.restart());
-  }
-
-  restart() {
-    this.BaseballGame.makeComputerNumbers();
-    this.BaseballGameView.resetInput();
-    this.BaseballGameView.resetResult();
-    this.BaseballGameView.toggleButtons(true);
-  }
-
-  gameStart() {
-    const input = document.querySelector("#user-input");
-    const submitButton = document.querySelector("#submit");
-
-    this.BaseballGame.makeComputerNumbers();
-
-    submitButton.addEventListener('click', (event) => {
-        event.preventDefault();
-
-        const userInput = input.value;
-        const userNumbers = this.NumberUtils.inputConvertNumbers(userInput);
-
-        if (!this.validateUserInput(userInput, userNumbers)) {
-            return;
-        }
-
-        const result = this.BaseballGame.play(userNumbers);
-        this.BaseballGameView.printResult(result);
-
-        if (this.NumberUtils.isPlayerWinner(result)){
-        this.endGame(result);
-        }
-    });
-  }
-
-  validateUserInput(userInput, userNumbers) {
-    if (!this.NumberUtils.validateNumbers(userInput)) {
-        alert("잘못된 입력입니다. 중복되지 않는 서로 다른 3개의 숫자를 입력하세요.");
-        return false;
+    constructor() {
+        this.NumberUtils = new NumberUtils();
+        this.BaseballGameView = new BaseballGameView();
+        this.BaseballGame = new BaseballGame();
+        this.gameStart();
     }
-    else if(!this.NumberUtils.validateDifferentNumber(userNumbers)) {
-        alert('잘못된 입력입니다. 1부터 9까지의 숫자만 입력하세요.');
-        return false;
+
+    endGame(result) {
+        setTimeout(function() {
+            alert(result + " 축하드립니다!");
+        }, 1);
+
+        const restartButton = document.querySelector("#game-restart-button");
+
+        this.BaseballGameView.toggleButtons(false);
+
+        restartButton.addEventListener('click', () => this.restart());
     }
-    return true;
-  }
+
+    restart() {
+        this.BaseballGameView.resetInput();
+        this.BaseballGameView.resetResult();
+        this.BaseballGameView.toggleButtons(true);
+        this.gameStart();
+    }
+
+    gameStart() {
+        const input = document.querySelector("#user-input");
+        const submitButton = document.querySelector("#submit");
+
+        this.BaseballGame.makeComputerNumbers();
+
+        submitButton.addEventListener('click', (event) => {
+            event.preventDefault();
+
+            const userInput = input.value;
+            const userNumbers = this.NumberUtils.inputConvertNumbers(userInput);
+
+            if (!this.validateUserInput(userInput, userNumbers)) {
+                return;
+            }
+
+            const result = this.BaseballGame.play(userNumbers);
+            this.BaseballGameView.printResult(result);
+
+            if (this.NumberUtils.isPlayerWinner(result)){
+                this.endGame(result);
+            }
+        });
+    }
+
+    validateUserInput(userInput, userNumbers) {
+        if (!this.NumberUtils.validateNumbers(userInput)) {
+            alert("잘못된 입력입니다. 중복되지 않는 서로 다른 3개의 숫자를 입력하세요.");
+            return false;
+        }
+        else if(!this.NumberUtils.validateDifferentNumber(userNumbers)) {
+            alert('잘못된 입력입니다. 1부터 9까지의 숫자만 입력하세요.');
+            return false;
+        }
+        return true;
+    }
 }
