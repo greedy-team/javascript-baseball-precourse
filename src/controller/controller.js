9f85f1908d938b6cffa4728bf4af873e53b9ee3d
songhj10207@naver.com <songhj10207@naver.com>
2024-10-01 13:57:47 +0900

    feat: controller 기능 구현

---
+++ src/controller/controller.js
@@ -0,0 +1,41 @@
+import verifyUserNumbers from '../utils/verifyUserNumbers.js';
+export default class Controller {
+  constructor(view, model) {
+    this.view = view;
+    this.model = model;
+    this.view.startGame(this.startGame.bind(this));
+  }
+
+  createComputerNumbers() {
+    this.model.createComputerNumbers();
+    const { computerNumbers } = this.model;
+    return computerNumbers;
+  }
+
+  countStrikeAndBall(computerNubers, userNumbers) {
+    this.model.countStrikeAndBall(computerNubers, userNumbers);
+  }
+
+  renderResultMessage() {
+    const resultMessage = this.model.createResultMessage();
+    if (resultMessage === '3스트라이크') {
+      this.view.restartGame();
+    } else {
+      this.view.renderResult(resultMessage);
+    }
+  }
+
+  startGame() {
+    const computerNubers = this.createComputerNumbers();
+    const userInput = this.view.userInput.value;
+    this.model.getUserNumber(userInput);
+    const { userNumbers } = this.model;
+    if (!verifyUserNumbers.validateNumber(userNumbers)) {
+      return;
+    }
+    console.log(computerNubers);
+    this.countStrikeAndBall(computerNubers, userNumbers);
+    this.renderResultMessage();
+    this.model.resetCount();
+  }
+}
