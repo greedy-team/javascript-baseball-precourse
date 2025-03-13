1825d2431ac74792dae37fbf5f05355eb053eabc
ChangwooJ <wjdckddn99@naver.com>
2025-03-13 23:11:06 +0900

    Feat: Html 요소 할당

---
+++ src/index.js
@@ -0,0 +1,6 @@
+// html 요소 할당
+const button = document.getElementById("submit");
+const resultMessage = document.getElementById("result");
+const restartBt = document.getElementById("game-restart-button");
+// 초기화 버튼 제거
+restartBt.style.display = "none";
