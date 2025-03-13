b5fee2703bcad78216ee5c06d9227410472decfc
ChangwooJ <wjdckddn99@naver.com>
2025-03-13 23:12:33 +0900

    Feat: 컴퓨터 랜덤 숫자 생성 및 유효성 검사 메서드 구현

--- src/index.js
+++ src/index.js
@@ -4,3 +4,22 @@ const resultMessage = document.getElementById("result");
 const restartBt = document.getElementById("game-restart-button");
 // 초기화 버튼 제거
 restartBt.style.display = "none";
+
+// 시작시 랜덤 숫자 생성 함수 호출
+let pitcher = randomString();
+
+// 컴퓨터의 랜덤 숫자 생성 함수
+function randomString() {
+    let vaildString = "";
+    while(vaildString.length < 3){
+        let randomNumber = MissionUtils.Random.pickNumberInRange(1, 9);
+        vaildString = findVaildNumber(vaildString, randomNumber);
+    }
+    return vaildString;
+} 
+
+// 숫자의 중복 검사 함수
+function findVaildNumber(vaildString, randomNumber) {
+    if(vaildString.includes(randomNumber)) return vaildString;
+    else return vaildString.concat(randomNumber);
+}
