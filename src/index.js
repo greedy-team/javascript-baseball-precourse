0eea05df3b2fd5f12a17d150c572474d30d37ab2
Shin Jihoon <tobeabestns@naver.com>
2025-03-14 01:55:38 +0900

    feat: 유효한 입력값을 판별하는 기능

--- src/index.js
+++ src/index.js
@@ -48,3 +48,20 @@ function countBall(pitcher, batter) {
     }
     return count;
 }
+
+function isValidInput(inputNumber) {
+    if (!isNumeric(inputNumber) || !isDuplicate(inputNumber) || inputNumber.length !== 3) {
+        alert("잘못된 형식으로 입력하셨습니다.");
+        return false;
+    }
+    return true;
+}
+
+function isNumeric(inputNumber) {
+    return /^[0-9]+$/.test(inputNumber);
+}
+
+function isDuplicate(inputNumber) {
+    const uniqueChars = new Set(inputNumber);
+    return uniqueChars.size == inputNumber.length;
+}
