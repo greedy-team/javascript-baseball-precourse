27e31b5e9ddedab841a7985eaf09c4b1abe1aca0
gxuoo <rbdud189@naver.com>
2025-03-14 21:13:12 +0900

    feat: 사용자 입력 유효성 검사 구현
    사용자가 입력한 값에 대한 3가지 유효성 검사 기능 구현

--- src/index.js
+++ src/index.js
@@ -21,6 +21,29 @@ function gameStart(event) {
     event.preventDefault();
 
     const user = userInput.value.trim();
+    if (!checkValidation(user)) {
+        alert("잘못된 값을 입력했습니다! 다시 입력해주세요.");
+        return;
+    }
+
+function checkValidation(value) {
+    if(!checkIsNumber(value)) return false; 
+    if(!checkDuplicate(value)) return false;
+    if(!checkNumberLength(value)) return false;
+    return true;
+}
+
+function checkIsNumber(value) {
+    return /^[1-9]{3}$/.test(value);    
+}
+
+function checkDuplicate(value) {
+    return new Set(value).size === 3;
+}
+
+function checkNumberLength(value) {
+    return value.length === 3;
+}
 }
 
 function showRestartButton() {
