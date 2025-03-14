355ec147dda0eddcbfeb8f0054f3ab3b879f73aa
gxuoo <rbdud189@naver.com>
2025-03-14 21:11:39 +0900

    feat: 게임 진행 함수 구현
    각 버튼 별, eventListener 추가하여 함수가 실행되도록 gamestart(), restartGame() 구현

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
+const userInput = document.querySelector("#user-input");
+const submitButton = document.querySelector("#submit");
+const resultMessage = document.querySelector("#result");
+const restartButton = document.querySelector("#game-restart-button");
+
 let answer = createRandomNumber();
+
+submitButton.addEventListener('click', gameStart);
+restartButton.addEventListener('click', restartGame);
+
 function createRandomNumber() {
     const numbers = [];
     while (numbers.length < 3) {
@@ -6,4 +15,29 @@ function createRandomNumber() {
         if (!numbers.includes(num)) numbers.push(num);
     }
     return numbers.join("");
+}
+
+function gameStart(event) {
+    event.preventDefault();
+
+    const user = userInput.value.trim();
+}
+
+function showRestartButton() {
+    submitButton.disabled = true;
+    restartButton.disabled = false;
+    restartButton.style.display = "block";
+}
+
+function showSubmitButton() {
+    submitButton.disabled = false;
+    restartButton.disabled = true;
+    restartButton.style.display = "none";
+}
+
+function restartGame() {
+    answer = createRandomNumber();
+    resultMessage.innerHTML = "";
+    userInput.value = "";
+    showSubmitButton();
 }
