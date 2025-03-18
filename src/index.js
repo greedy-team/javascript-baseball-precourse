14de235fd9344b4311162851da252f33f76c309d
INSANE-P <chanbin0626@gmail.com>
2025-03-18 12:30:24 +0900

    refactor: 호출 순서에 맞춰 함수 선언 순서 재배치
    style: 백틱을 따옴표로 변경하여 코드 스타일 통일

--- src/index.js
+++ src/index.js
@@ -3,8 +3,11 @@ import BaseballGame from "./BaseballGame.js";
 const restartButton = document.querySelector("#game-restart-button");
 const resultElement = document.querySelector("#result");
 const userInputElement = document.querySelector("#user-input");
+
 let computerNumbers;
 const baseballGame = new BaseballGame();
+
+const isValidInput = (input) => /^[1-9]{3}$/.test(input) && new Set(input).size === 3;
 
 const generateRandomNumbers = () => {
   const numbers = new Set();
@@ -24,6 +27,43 @@ const clearInput = () => {
   userInputElement.value = "";
 };
 
+const showRestartButton = () => {
+  restartButton.hidden = false;
+};
+
+const hideRestartButton = () => {
+  restartButton.hidden = true;
+};
+
+const updateGameResult = (result) => {
+  if (result === "정답") {
+    resultElement.innerHTML = 
+      "🎉<strong>정답을 맞추셨습니다!</strong>🎉<br><br>게임을 다시 시작하시겠습니까?<br>";
+    showRestartButton();
+  } else {
+    resultElement.textContent = result;
+  }
+};
+
+const handleUserInput = () => {
+  const userInput = userInputElement.value;
+  if (!isValidInput(userInput)) {
+    alert("⚠️ 잘못된 입력입니다! 1~9까지의 수를 중복 없이 3개 입력해주세요.");
+    return;
+  }
+
+  const userNumbers = userInput.split("").map(Number);
+  const result = baseballGame.play(computerNumbers, userNumbers);
+
+  updateGameResult(result);
+};
+
+const restartGame = () => {
+  computerNumbers = generateRandomNumbers();
+  clearInput();
+  clearResult();
+};
+
 const initEventListeners = () => {
   document.querySelector("#submit").addEventListener("click", (event) => {
     event.preventDefault();
@@ -37,45 +77,6 @@ const gameStart = () => {
   computerNumbers = generateRandomNumbers();
   clearResult();
   initEventListeners();
-};
-
-const handleUserInput = () => {
-  const userInput = userInputElement.value;
-  if (!isValidInput(userInput)) {
-    alert("⚠️ 잘못된 입력입니다! 1~9까지의 수를 중복없이 3개 입력해주세요.");
-    return;
-  }
-
-  const userNumbers = userInput.split("").map(Number);
-  const result = baseballGame.play(computerNumbers, userNumbers);
-
-  updateGameResult(result);
-};
-
-const updateGameResult = (result) => {
-  if (result === "정답") {
-    resultElement.innerHTML =
-      `🎉<strong>정답을 맞추셨습니다!</strong>🎉<br><br>게임을 다시 시작하시겠습니까?<br>`;
-    showRestartButton();
-  } else {
-    resultElement.textContent = result;
-  }
-};
-
-const isValidInput = (input) => /^[1-9]{3}$/.test(input) && new Set(input).size === 3;
-
-const restartGame = () => {
-  computerNumbers = generateRandomNumbers();
-  clearInput();
-  clearResult();
-};
-
-const showRestartButton = () => {
-  restartButton.hidden = false;
-};
-
-const hideRestartButton = () => {
-  restartButton.hidden = true;
 };
 
 gameStart();
