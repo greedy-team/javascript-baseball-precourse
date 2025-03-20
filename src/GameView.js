53e2ef8553edc1662997b25ebac6eb4edf52ae4c
zldn109 <ss771009@naver.com>
2025-03-21 02:44:32 +0900

    refactor: 주석 삭제

--- src/GameView.js
+++ src/GameView.js
@@ -1,6 +1,6 @@
-//View
-import { gameReplay } from "./GameController.js"
+//뷰
 
+import { gameReplay } from "./GameController.js"
 export default class GameView {
     displaySuccessMessage() {
         document.querySelector("#result").style.display = "none";
@@ -28,32 +28,4 @@ export default class GameView {
         gameResult.style.display = "block";
         gameResult.textContent = comparisionResult;
     }
-
 }
-
-// export function displaySuccessMessage() {
-//     document.querySelector("#result").style.display = "none";
-//     document.querySelector(".success").style.display = "block";
-
-//     const submitBtn = document.querySelector("#submit");
-//     submitBtn.disabled = true;
-
-//     const replay = document.querySelector("#game-restart-button");
-//     replay.addEventListener("click", (event) => {
-//         event.preventDefault();
-//         gameReplay();
-//     });
-// }
-
-// export function resetGameUI() {
-//     const submitBtn = document.querySelector("#submit");
-//     submitBtn.disabled = false;
-
-//     document.querySelector(".success").style.display = "none";
-// }
-
-// export function displayResultMessage(comparisionResult) {
-//     let gameResult = document.querySelector("#result");
-//     gameResult.style.display = "block";
-//     gameResult.textContent = comparisionResult;
-// }
