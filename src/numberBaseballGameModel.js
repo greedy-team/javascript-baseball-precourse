bba91e348f7237461c0d6ea159f9f08cec2a1b7a
gxuoo <rbdud189@naver.com>
2025-03-20 22:26:13 +0900

    refactor: Model Refactoring
    Model에 해당되는 메서드와 변수를 하나의 클래스로 만들었고, 기존의 BaseballGame 클래스에서 사용자 입력에 대한 검증 메서드를 추가했습니다.

--- src/numberBaseballGameModel.js
+++ src/numberBaseballGameModel.js
@@ -1,2 +1,66 @@
+import { CORRECT, NOTHING, NUMBER_OF_MAXIMUM_LENGTH, STRIKE_COUNTS_FOR_ANSWER, STRING_LENGTH_ERROR_MESSAGE, DUPLICATE_ERROR_MESSAGE } from './constants.js';
+
 export default class numberBaseballGameModel {
+    #answer;
+
+    constructor() {
+        this.setAnswer();
+    }
+
+    setAnswer() {
+        this.#answer = this.#createRandomNumberToString();
+    }
+
+    getAnswer() {
+        return this.#answer;
+    }
+
+    checkUserInputValidation(value, messages) {
+        let isValid = true;
+        if (!this.checkThreeDigitStringWithoutZero(value)) {
+            if (!messages.includes(STRING_LENGTH_ERROR_MESSAGE)) messages.push(STRING_LENGTH_ERROR_MESSAGE);
+            isValid = false;
+        }
+        if (!this.checkDuplicatesInString(value)) {
+            if (!messages.includes(DUPLICATE_ERROR_MESSAGE)) messages.push(DUPLICATE_ERROR_MESSAGE);
+            isValid = false;
+        }
+        return isValid;
+    }
+
+    checkThreeDigitStringWithoutZero(value) {
+        return /^[1-9]{3}$/.test(value);
+    }
+
+    checkDuplicatesInString(value) {
+        return new Set(value).size === value.length;
+    }
+
+    #createRandomNumberToString() {
+        const numbers = [];
+        while (numbers.length < NUMBER_OF_MAXIMUM_LENGTH) {
+            const num = MissionUtils.Random.pickNumberInRange(1, 9);
+            if (!numbers.includes(num)) numbers.push(num);
+        }
+        return numbers.join('');
+    }
+
+    play(user) {
+        let strike = 0;
+        let ball = 0;
+
+        for (let i = 0; i < NUMBER_OF_MAXIMUM_LENGTH; i++) {
+            if (this.#answer[i] === user[i]) strike++;
+            else if (user.includes(this.#answer[i])) ball++;
+        }
+        return this.getResultMessage(strike, ball);
+    }
+
+    getResultMessage(strike, ball) {
+        if (strike === STRIKE_COUNTS_FOR_ANSWER) return CORRECT;
+        if (strike === 0 && ball === 0) return NOTHING;
+        if (ball === 0) return `${strike}스트라이크`;
+        if (strike === 0) return `${ball}볼`;
+        return `${ball}볼 ${strike}스트라이크`;
+    }
 }
