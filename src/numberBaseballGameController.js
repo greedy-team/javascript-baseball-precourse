44eed9be8507135ccef6a7c5297d1b1a53af663c
gxuoo <rbdud189@naver.com>
2025-03-20 22:23:40 +0900

    reafactor: Controller Refactoring
    Controller에 해당되는 메서드를 모아 하나의 Claas로 refactoring 했습니다.
    
    기존에 eventListener를 add하는 방식이 리팩토링 과정에서 this 함수가 클래스 인스턴스를 가르키지 않고 HTML Element를 가리키는 문제가 있어, 화살표 함수로 변경했습니다.

--- src/numberBaseballGameController.js
+++ src/numberBaseballGameController.js
@@ -1,10 +1,44 @@
 import numberBaseballGameView from './numberBaseballGameView.js';
 import numberBaseballGameModel from './numberBaseballGameModel.js';
+
 export default class numberBaseballGameController {
     #numberBaseballGameView;
     #numberBaseballGamemModel;
+
     constructor() {
         this.#numberBaseballGamemModel = new numberBaseballGameModel();
         this.#numberBaseballGameView = new numberBaseballGameView();
+        this.#numberBaseballGameView.setFocusOnUserInput();
+        this.setupButtonEventListener();
+    }
+
+    setupButtonEventListener() {
+        const submitButton = this.#numberBaseballGameView.getSubmitButtonElement();
+        const restartButton = this.#numberBaseballGameView.getrestartButtonElement();
+        submitButton.addEventListener('click', (event) => this.gameStart(event));
+        restartButton.addEventListener('click', () =>  this.restartGame());
+    }
+
+    gameStart(event) {
+        event.preventDefault();
+
+        const userInput = this.#numberBaseballGameView.getUserInputElement();
+        const user = userInput.value.trim();
+        const messages = [];
+
+        if (!this.#numberBaseballGamemModel.checkUserInputValidation(user, messages)) {
+            this.#numberBaseballGameView.showAlertMessage(messages);
+            return;
+        }
+        const message = this.#numberBaseballGamemModel.play(user);
+        this.#numberBaseballGameView.showMessage(message);
+    }
+
+    restartGame() {
+        this.#numberBaseballGamemModel.setAnswer();
+        this.#numberBaseballGameView.clearResultMessageValue();
+        this.#numberBaseballGameView.clearUserInputValue();
+        this.#numberBaseballGameView.toggleButtonState();
+        this.#numberBaseballGameView.toggleUserInputState();
     }
 }
