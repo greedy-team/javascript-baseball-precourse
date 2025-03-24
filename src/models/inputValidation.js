469b9a3ade71756994be896b016b3b203524054e
Shin Jihoon <tobeabestns@naver.com>
2025-03-24 13:13:33 +0900

    refactor: isDuplicate 메서드명 리팩토링

--- src/models/inputValidation.js
+++ src/models/inputValidation.js
@@ -1,12 +1,12 @@
 export function isValidInput(inputNumber) {
-    return isNumber1To9(inputNumber) && isDuplicate(inputNumber) && inputNumber.length === 3;
+    return isNumber1To9(inputNumber) && isStringNonDuplicate(inputNumber) && inputNumber.length === 3;
 }
 
 function isNumber1To9(inputNumber) {
     return /^[1-9]+$/.test(inputNumber);
 }
 
-function isDuplicate(inputNumber) {
+function isStringNonDuplicate(inputNumber) {
     const uniqueChars = new Set(inputNumber);
     return uniqueChars.size === inputNumber.length;
 }
