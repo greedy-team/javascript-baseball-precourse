8d3e712a4be4af5b1d77040c007ba56f76f08ea6
Johncakes <johnnygamesover9000@gmail.com>
2025-09-22 01:27:41 +0900

    refactor: eslint 코드 포매팅 적용

--- src/index.js
+++ src/index.js
@@ -1,26 +1,24 @@
-import BaseBallGame from "./baseBallGame.js";
-import View from "./view.js";
+import BaseBallGame from './baseBallGame.js';
+import View from './view.js';
 
 const baseBallGame = BaseBallGame();
 const view = View({
-    onSubmit: (userInput) => {
-        try {
-            const result = baseBallGame.handleUserInput(userInput);
-            if (result === "win"){
-                view.displayGameWinView();
-            }
-            else{
-                view.updateView(result);
-            }
-        } catch (error) {
-            view.displayErrorView();
-        }
-        
-    },
-    onRestart: () => {
-        baseBallGame.startGame();
-        view.displayGameStartView();
+  onSubmit: (userInput) => {
+    try {
+      const result = baseBallGame.handleUserInput(userInput);
+      if (result === 'win') {
+        view.displayGameWinView();
+      } else {
+        view.updateView(result);
+      }
+    } catch (error) {
+      view.displayErrorView();
     }
+  },
+  onRestart: () => {
+    baseBallGame.startGame();
+    view.displayGameStartView();
+  },
 });
 
 view.displayGameStartView();
