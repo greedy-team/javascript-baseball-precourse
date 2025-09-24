6f830f44550d860e8442590752cabf11130eb31a
Sim Hyeok <johnnygamesover9000@gmail.com>
2025-09-24 23:09:10 +0900

    [2주차] 심혁 숫자 야구 게임 MVC 패턴 적용 제출합니다.  (#39)
    * docs: ui 구성 텍스트 모호성 수정
    
    * refactor: EOF 없는 파일 수정
    
    * refactor: Click Event에서 Submit Event으로 수정
    
    * fix: 게임 승리시 성공 텍스트 없는 오류 수정
    
    * feat: eslint 패키지 설치
    
    * refactor: eslint 코드 포매팅 적용
    
    * refactor: eslint 추가 적용
    
    - import 과정에 '.js' 제거
    
    * refactor:  onSubmit event변경으로 인한 변수 네이밍 수정refactor: subsfsfsdfsdfdfrefactor:  onSubmit event변경으로 인한 변수 네이밍 수정
    
    submitButton에서 userForm으로 변경
    
    * chore: baseBallGame내 eslint 오류 수정

--- src/baseBallGame.js
+++ src/baseBallGame.js
@@ -1,69 +1,70 @@
-export default function BaseballGame(){
-    let pitchNumber = "";
-    
-    const generateRandomNum= () => {
-        const digits = new Set();
+import MissionUtils from '@woowacourse/mission-utils';
 
-        while (digits.size< 3) {
-            const randomDigit = MissionUtils.Random.pickNumberInRange(0, 9);
-            digits.add(randomDigit);
-        } 
+export default function BaseballGame() {
+  let pitchNumber = '';
 
-        return [...digits].join(''); 
+  const generateRandomNum = () => {
+    const digits = new Set();
+
+    while (digits.size < 3) {
+      const randomDigit = MissionUtils.Random.pickNumberInRange(0, 9);
+      digits.add(randomDigit);
     }
 
-    const startGame = () => {
-        pitchNumber = generateRandomNum();
+    return [...digits].join('');
+  };
+
+  const startGame = () => {
+    pitchNumber = generateRandomNum();
+  };
+
+  const checkValidInput = (userInput) => {
+    const regex = /^\d{3}$/;
+    if (!regex.test(userInput)) return false;
+
+    const digits = userInput.split('');
+    const uniqueDigits = new Set(digits);
+    return uniqueDigits.size === 3;
+  };
+
+  const countStrikesAndBalls = (computerInputNumbers, userInputNumbers) => {
+    const computerInputList = computerInputNumbers.split('');
+    const userInputList = userInputNumbers.split('');
+
+    let strikes = 0;
+    let balls = 0;
+
+    userInputList.forEach((num, index) => {
+      if (num === computerInputList[index]) {
+        strikes += 1;
+      } else if (computerInputList.includes(num)) {
+        balls += 1;
+      }
+    });
+
+    return { strikes, balls };
+  };
+
+  const formatResult = (strikes, balls) => {
+    if (strikes === 3) {
+      return 'win';
+    } if (strikes === 0 && balls === 0) {
+      return '낫싱';
     }
+    return `${balls}볼 ${strikes}스트라이크`;
+  };
 
-    const handleUserInput = (userInput) => {
-        if (checkValidInput(userInput)){
-            return play(pitchNumber, userInput);
-        }
-        throw new Error("Invalid input");
+  const play = (computerInputNumbers, userInputNumbers) => {
+    const { strikes, balls } = countStrikesAndBalls(computerInputNumbers, userInputNumbers);
+    return formatResult(strikes, balls);
+  };
+
+  const handleUserInput = (userInput) => {
+    if (checkValidInput(userInput)) {
+      return play(pitchNumber, userInput);
     }
+    throw new Error('Invalid input');
+  };
 
-    const checkValidInput = (userInput) => {
-        const regex = /^\d{3}$/;
-        if (!regex.test(userInput)) return false;
-
-        const digits = userInput.split('');
-        const uniqueDigits= new Set(digits);
-        return uniqueDigits.size === 3;
-    }
-    
-    const countStrikesAndBalls = (computerInputNumbers, userInputNumbers) => {
-        const computerInputList = computerInputNumbers.split('');
-        const userInputList = userInputNumbers.split('');
-
-        let strikes = 0;
-        let balls = 0;
-
-        userInputList.forEach((num, index) => {
-            if (num === computerInputList[index]) {
-                strikes++;
-            }
-            else if (computerInputList.includes(num)) {
-                balls++;
-            }
-        });
-
-        return { strikes, balls };
-    }
-
-    const formatResult = (strikes, balls) => {
-        if (strikes === 3) {
-            return "win";
-        } else if (strikes === 0 && balls === 0) {
-            return "낫싱";
-        }
-        return `${balls}볼 ${strikes}스트라이크`;
-    }
-
-    const play = (computerInputNumbers, userInputNumbers) =>{
-        const { strikes, balls } = countStrikesAndBalls(computerInputNumbers, userInputNumbers);
-        return formatResult(strikes, balls);
-    }
-
-    return {handleUserInput, startGame};
+  return { handleUserInput, startGame };
 }
--- src/view.js
+++ src/view.js
@@ -1,42 +1,45 @@
-export default function View({ onSubmit, onRestart }){
-    const submitButton = document.getElementById("submit");
-    const restartButton = document.getElementById("game-restart-button");
-    const restartSuggestionText = document.getElementById("restart-suggestion-text");
-    const resultText = document.getElementById("result");
-    const userInput = document.getElementById("user-input");
+export default function View({ onSubmit, onRestart }) {
+  const userForm = document.getElementById('form');
+  const restartButton = document.getElementById('game-restart-button');
+  const restartSuggestionText = document.getElementById('restart-suggestion-text');
+  const resultText = document.getElementById('result');
+  const userInput = document.getElementById('user-input');
 
-    submitButton.addEventListener("click", (event) => {
-        event.preventDefault();
-        if(onSubmit){
-            onSubmit(userInput.value);
-        }
-    });
-
-    restartButton.addEventListener("click", (event) => {
-        event.preventDefault();
-        if(onRestart){
-            onRestart();
-        }
-    });
-
-    const displayGameStartView = () => {
-        restartButton.style.display = 'none';
-        restartSuggestionText.style.display = 'none';
-        resultText.innerText = '';
+  userForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    if (onSubmit) {
+      onSubmit(userInput.value);
     }
+  });
 
-    const updateView = (result) => {
-        resultText.innerText = result;
+  restartButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    if (onRestart) {
+      onRestart();
     }
+  });
 
-    const displayGameWinView = () => {
-        restartButton.style.display = 'block';
-        restartSuggestionText.style.display = 'block';
-    }
+  const displayGameStartView = () => {
+    restartButton.style.display = 'none';
+    restartSuggestionText.style.display = 'none';
+    resultText.innerText = '';
+  };
 
-    const displayErrorView = () => {
-        alert("잘못된 입력입니다. 세 자리 숫자를 입력해주세요.");
-    }
+  const updateView = (result) => {
+    resultText.innerText = result;
+  };
 
-    return { displayGameStartView,  updateView,  displayGameWinView, displayErrorView};
+  const displayGameWinView = () => {
+    resultText.innerText = '🎉 정답을 맞추셨습니다! 🎉';
+    restartButton.style.display = 'block';
+    restartSuggestionText.style.display = 'block';
+  };
+
+  const displayErrorView = () => {
+    alert('잘못된 입력입니다. 세 자리 숫자를 입력해주세요.');
+  };
+
+  return {
+    displayGameStartView, updateView, displayGameWinView, displayErrorView,
+  };
 }
