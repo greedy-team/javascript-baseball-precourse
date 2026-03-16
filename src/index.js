7ed38c3ece7ba5514e8fdf3c0134a0f55508b714
gyuminJJANG <ggavwu789@gmail.com>
2026-03-17 02:28:22 +0900

    feat: 1에서 9 사이의 서로 다른 임의의 수 3개 생성하는 기능 구현

--- src/index.js
+++ src/index.js
@@ -11,8 +11,22 @@ export default class BaseballGame {
         // 재시작 버튼 숨기기
         this.restart.style.display = 'none';
         
+       // 초기 상태 설정: 컴퓨터 숫자 생성
+        this.computerNumbers = this.generateComputerNumbers();
 
+        // 시스템 작동 시작: 이벤트(사용자 신호) 받을 준비
+        this.initEventListeners();
     }
+    // 컴퓨터 숫자 생성 (중복 없는 난수 3개)
+    generateComputerNumbers() {
+        const numbers = [];
+        while (numbers.length < 3) {
+            const number = MissionUtils.Random.pickNumberInRange(1, 9);
+            if (!numbers.includes(number)) numbers.push(number);
+        }
+        return numbers;
+    }   
+    
 }
 
 new BaseballGame();
