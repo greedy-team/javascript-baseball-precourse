9ef620d3e0cf1ead3b33ef04aeae9b1d19772cb0
EM-H20 <ghd0106@gmail.com>
2026-03-17 11:31:54 +0900

    fix: 잘못된 import 수정

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import BaseballGameView from "./views/BaseballGameView";
-import BaseballGameModel from "./models/BaseballGameModel";
-import BaseballGameController from "./controllers/BaseballGameController";
+import BaseballGameView from "./views/BaseballGameView.js";
+import BaseballGameModel from "./models/BaseballGameModel.js";
+import BaseballGameController from "./controllers/BaseballGameController.js";
 
 // MVC 패턴을 적용하여 게임을 구성
 const model = new BaseballGameModel();
