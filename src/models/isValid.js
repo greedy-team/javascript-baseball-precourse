4d1098b9c5dcc239e93b51bc0c6fae570d09ed57
gyuminJJANG <ggavwu789@gmail.com>
2026-03-20 14:55:23 +0900

    fix: 정규표현식을 도입하여 소수점 및 예외 케이스 유효성 검사 보강

--- src/models/isValid.js
+++ src/models/isValid.js
@@ -1,29 +1,38 @@
+// 유효성 검사
 export function isValid(userValue){
-    // 유효성 검사
+
+    const regex = /[1-9]{3}$/;
+
+    if()
+
+        // 숫자인지 확인
         if(isNaN(userValue)){
             alert('숫자만 입력해주세요!');
             return;
         }
-        // 숫자인지 확인
         
+        // 3자리인지 확인
         if(userValue.length !== 3){
             alert('반드시 3자리 숫자를 입력해주세요!');
             return;
         }
-        // 3자리인지 확인
 
+        // 중복을 허용하지 않는 Set 자료형의 특성을 활용
         if(new Set(userValue).size !== 3){
             alert('중복된 숫자가 있습니다!');
             return;
         }
-        // 중복을 허용하지 않는 Set 자료형의 특성을 활용
 
+        // 입력값에 '0'이 있는지 확인 -> 있으면 alert
         if(userValue.includes('0')){
             alert('0은 포함될 수 없습니다. 1~9 사이의 숫자를 입력해주세요.');
             return;
         }
-        // 입력값에 '0'이 있는지 확인 -> 있으면 alert
+
+        if(!Number.isInteger(Number(userValue))){
+            alert('소수점은 입력할 수 없습니다!');
+            return;
+        }
 
         return true;
-        
 }
